fe46d56df5429fadd01efceba9207d2dc8105dcf
Jun Wang <jun.wang@arubastudio.com>
2013-07-31 19:29:10 +0800

    remove useless

--- AMRRecording/RecordAudio.m
+++ AMRRecording/RecordAudio.m
@@ -35,11 +35,8 @@
     self = [super init];
     if (self)
     {
-        //Instanciate an instance of the AVAudioSession object.
+        /*
         AVAudioSession * audioSession = [AVAudioSession sharedInstance];
-        //Setup the audioSession for playback and record. 
-        //We could just use record and then switch it to playback leter, but
-        //since we are going to do both lets set it up once.
         [audioSession setCategory:AVAudioSessionCategoryPlayAndRecord error: &error];
         
         UInt32 audioRouteOverride = kAudioSessionOverrideAudioRoute_Speaker;
@@ -47,8 +44,8 @@
 								 sizeof (audioRouteOverride),
 								 &audioRouteOverride);
         
-        //Activate the session
         [audioSession setActive:YES error: &error];
+         */
     }
     return self;
 }
@@ -56,31 +53,6 @@
 #pragma mark- Public Methods
 -(void) startRecord
 {
-    //Begin the recording session.
-    //Error handling removed.  Please add to your own code.
-    
-    //Setup the dictionary object with all the recording settings that this
-    //Recording sessoin will use
-    //Its not clear to me which of these are required and which are the bare minimum.
-    //This is a good resource: http://www.totodotnet.net/tag/avaudiorecorder/
-    //		NSMutableDictionary* recordSetting = [[NSMutableDictionary alloc] init];
-    //		[recordSetting setValue :[NSNumber numberWithInt:kAudioFormatAppleIMA4] forKey:AVFormatIDKey];
-    //		[recordSetting setValue:[NSNumber numberWithFloat:44100.0] forKey:AVSampleRateKey];
-    //		[recordSetting setValue:[NSNumber numberWithInt: 2] forKey:AVNumberOfChannelsKey];
-    
-    //    NSDictionary *recordSetting =
-    //    [[NSDictionary alloc] initWithObjectsAndKeys:
-    //
-    //     [NSNumber numberWithFloat:8000.00], AVSampleRateKey,
-    //
-    //     [NSNumber numberWithInt:kAudioFormatiLBC], AVFormatIDKey,
-    //
-    //     [NSNumber numberWithInt:1], AVNumberOfChannelsKey,
-    //
-    //     [NSNumber numberWithInt:AVAudioQualityMax], AVEncoderAudioQualityKey,
-    //
-    //     nil];
-    
     NSDictionary *recordSetting = [NSDictionary dictionaryWithObjectsAndKeys:
                                    [NSNumber numberWithInt:kAudioFormatLinearPCM], AVFormatIDKey,
                                    //[NSNumber numberWithFloat:44100.0], AVSampleRateKey,
@@ -93,32 +65,17 @@
                                    [NSNumber numberWithBool:NO], AVLinearPCMIsBigEndianKey,
                                    nil];
     
-    //Now that we have our settings we are going to instanciate an instance of our recorder instance.
-    //Generate a temp file for use by the recording.
-    //This sample was one I found online and seems to be a good choice for making a tmp file that
-    //will not overwrite an existing one.
-    //I know this is a mess of collapsed things into 1 call.  I can break it out if need be.
     recordedTmpFile = [NSURL fileURLWithPath:[NSTemporaryDirectory() stringByAppendingPathComponent: [NSString stringWithFormat: @"%.0f.%@", [NSDate timeIntervalSinceReferenceDate] * 1000.0, @"caf"]]];
     NSLog(@"Using Temp File called: %@",recordedTmpFile);
     
     
-    //Setup the recorder to use this file and record to it.
     self.recorder = [[[AVAudioRecorder alloc] initWithURL:recordedTmpFile settings:recordSetting error:&error] autorelease];
     recorder.meteringEnabled = YES;
     NSLog(@"1");
-    //Use the recorder to start the recording.
-    //Im not sure why we set the delegate to self yet.
-    //Found this in antother example, but Im fuzzy on this still.
-    [recorder setDelegate:self];
-    //We call this to start the recording process and initialize
-    //the subsstems so that when we actually say "record" it starts right away.
     [recorder prepareToRecord];
     NSLog(@"2");
-    //Start the actual Recording
     [recorder record];
     NSLog(@"3");
-    //There is an optional method for doing the recording for a limited time see
-    //[recorder recordForDuration:(NSTimeInterval) 10]
 }
 
 - (NSURL *) stopRecord
@@ -132,7 +89,6 @@
 -(void) play:(NSData*) data
 {
 	//Setup the AVAudioPlayer to play the file that we just recorded.
-    //在播放时，只停止
     if (avPlayer!=nil)
     {
         [self stopPlay];
